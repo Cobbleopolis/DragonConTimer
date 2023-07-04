40b2e598a809aa1ffb815b0452b8b0b0395fea51
Cobbleopolis <cobbleopolis@gmail.com>
2023-07-03 21:07:02 -0400

    Basic individual console quering

--- packages/client/src/components/ConsoleComponent.vue
+++ packages/client/src/components/ConsoleComponent.vue
@@ -1,13 +1,55 @@
 <template>
-    <div class="card">
-        <div class="card-body">
-            <p>{{ consoleId }}</p>
+    <div class="card mb-2">
+        <div class="card-header">
+            <span v-if="consoleReq.loading">Loading</span>
+            <span v-else>
+                {{ console.name }}
+            </span>
         </div>
+        <template v-if="consoleReq.loading">
+            <div class="card-body">
+                <LoadingAnimation />
+            </div>
+        </template>
+            <template v-else>
+                <p>{{ consoleId }}</p>
+            </template>
     </div>
 </template>
 
-<script setup>
-const props = defineProps({
-    consoleId: String
-})
+<script>
+import LoadingAnimation from './LoadingAnimation.vue'
+import { computed } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+export default {
+    props: {
+        consoleId: String
+    },
+    data() {
+        const consoleReq = useQuery(gql `
+        query ConsoleById($consoleId: MongoID!) {
+            consoleById(_id: $consoleId) {
+                checkoutWarning
+                games {
+                    name
+                    count
+                }
+                name
+            }
+        }`, this.$props)
+
+        // consoleReq.onResult(res => this.console = res.data['consoleById'])
+
+        const console = computed(() => consoleReq.result.value?.consoleById ?? {})
+
+        return {
+            consoleReq,
+            console,
+        }
+    },
+    components: { LoadingAnimation }
+}
+
 </script>
